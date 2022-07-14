61b4a34594b929551af8e3bb7944529f05191a77
R-Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2022-07-14 12:52:40 +0100

    add option for precision weighting

--- R/calculate_lambdas.R
+++ R/calculate_lambdas.R
@@ -1,21 +1,22 @@
 #' Calculate univariable lambda
 #'
-#' Calculates lambda (regression dilution coefficient) using repeated measurements data.
+#' Calculates lambda (regression dilution coefficient) using repeated measurements data. Data should be in wide format i.e. one row per participant with multiple columns for the different repeats. Different repeats should be indicated consistently by the same suffix.
 #'
 #' NOTE: uses mean value across all follow-up measurements with data
 #'
 #' @param var_name Name of variable to calculate lambda for
 #' @param suffix_baseline Suffix denoting a baseline measurement
 #' @param suffix_rep Suffix(es) denoting a follow-up measurement
-#' @param exclusion_indic Name of variable indicating row should be excluded for that repeat
+#' @param exclusion_indic Name of variable indicating row should be excluded for that repeat (should have same suffix as indicated in `suffix_baseline`, `suffix_rep`)
 #' @param data Dataset containing repeated measurements data
 #' @param covariates Covariates to adjust for in lambda estimation model
+#' @param precision_weighted Indicator of whether precision weighting should be used in model for follow up based on number of repeats
 #' @return List containing lambda estimate and its variance.
 #' @export
-calc_lambda <- function(var_name, data, suffix_baseline = "_s0", suffix_rep = c("_s1", "_s2", "_s3", "_s4"), exclusion_indic = "excluded", covariates = NULL){
+calc_lambda <- function(var_name, data, suffix_baseline = "_s0", suffix_rep = c("_s1", "_s2", "_s3", "_s4"), exclusion_indic = "excluded", covariates = NULL, precision_weighted = FALSE){
 
   # Checks of inputs
-  var_name_base <- paste0(var_name,  suffix_baseline)
+  var_name_base <- paste0(var_name, suffix_baseline)
   var_names_rep <- paste0(var_name, suffix_rep)
 
   for (var_loc in c(var_name_base, var_names_rep)){
@@ -25,12 +26,15 @@ calc_lambda <- function(var_name, data, suffix_baseline = "_s0", suffix_rep = c(
   }
 
   # Mean of relevant repeats
-  data$mean_reps_w_data <- calc_mean_reps(var_name = var_name, data = data, suffix_baseline = suffix_baseline, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic)
+  data$mean_reps_w_data <- calc_mean_reps(var_name = var_name, data = data, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic)
+
+  # Precision weights
+  precision_weights <- calc_precision_weights(data = data, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic, precision_weighted = precision_weighted)
 
   # Set up linear model
   covs <- paste0(c(var_name_base, covariates), collapse = "+")
   form <- as.formula(paste0("mean_reps_w_data ~", covs))
-  lambda_lin <- lm(form, data)
+  lambda_lin <- lm(form, data, weights = precision_weights)
 
   # Extract lambda and var
   lambda <- coef(lambda_lin)[[var_name_base]]
--- R/multivariable_adjustment.R
+++ R/multivariable_adjustment.R
@@ -10,9 +10,10 @@
 #' @param exclusion_indic Name of variable indicating row should be excluded for that repeat
 #' @param data Dataset containing repeated measurements data
 #' @param covariates Covariates to adjust for in lambda estimation model
+#' @param precision_weighted Indicator of whether precision weighting should be used in model for follow up based on number of repeats
 #' @return List of models for different outcomes
 #' @export
-calc_models_multi <- function(var_names, data, suffix_baseline = "_s0", suffix_rep = c("_s1", "_s2", "_s3", "_s4"), exclusion_indic = "excluded", covariates = NULL){
+calc_models_multi <- function(var_names, data, suffix_baseline = "_s0", suffix_rep = c("_s1", "_s2", "_s3", "_s4"), exclusion_indic = "excluded", covariates = NULL, precision_weighted = FALSE){
 
   # Checks of inputs
   var_names_base <- paste0(var_names,  suffix_baseline)
@@ -32,14 +33,23 @@ calc_models_multi <- function(var_names, data, suffix_baseline = "_s0", suffix_r
   cols <- c(covariates, var_names_base)
   X <- model.matrix(~ ., data = data[, cols])
 
+  # Precision weights for outcome
+  precision_weights <- calc_precision_weights(data = data, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic, precision_weighted = precision_weighted)
+
 
   for (var_name in var_names){
     # Mean of relevant repeat is outcome
-    data[, paste0("mean_reps_w_data", var_name)] <- calc_mean_reps(var_name = var_name, data = data, suffix_baseline = suffix_baseline, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic)
+    data[, paste0("mean_reps_w_data", var_name)] <- calc_mean_reps(var_name = var_name, data = data, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic)
+
     y <- data[, paste0("mean_reps_w_data", var_name)]
 
     # Calc model
-    lambda_lin <- lm.fit(X, y)
+    if (is.null(precision_weights)){
+      lambda_lin <- lm.fit(X, y)
+    }
+    else {
+      lambda_lin <- lm.wfit(X, y, w = precision_weights)
+    }
 
     # Append model to list and remove
     models_list[[var_name]] <- list(lambda_lin, cols)
@@ -52,22 +62,17 @@ calc_models_multi <- function(var_names, data, suffix_baseline = "_s0", suffix_r
 
 
 
-#' Calculates linear models for multivariable adjustment
+#' Calculates linear models for multivariable adjustment (direct multivariate regression calibration model)
 #'
-#' Calculates linear models for multivariable adjustment using repeated measurements data.
+#' Calculates multivariate linear model for multivariable adjustment using repeated measurements data.
 #'
 #' NOTE: uses mean value across all follow-up measurements with data
-#' NOTE: I think this is essentially redundant with
+#' NOTE: This is essentially redundant with `calc_models_multi`, but it's directly creating multivariate model rather than creating each univariate model (coefficients within each model should be same, though)
 #'
-#' @param var_names Name of variables to calculate models for
-#' @param suffix_baseline Suffix denoting a baseline measurement
-#' @param suffix_rep Suffix(es) denoting a follow-up measurement
-#' @param exclusion_indic Name of variable indicating row should be excluded for that repeat
-#' @param data Dataset containing repeated measurements data
-#' @param covariates Covariates to adjust for in lambda estimation model
-#' @return Multivariable model
+#' @inheritParams calc_models_multi
+#' @return Multivariate model
 #' @export
-calc_models_multi_single <- function(var_names, data, suffix_baseline = "_s0", suffix_rep = c("_s1", "_s2", "_s3", "_s4"), exclusion_indic = "excluded", covariates = NULL){
+calc_models_multi_single <- function(var_names, data, suffix_baseline = "_s0", suffix_rep = c("_s1", "_s2", "_s3", "_s4"), exclusion_indic = "excluded", covariates = NULL, precision_weighted = FALSE){
 
   # Checks of inputs
   var_names_base <- paste0(var_names,  suffix_baseline)
@@ -81,8 +86,12 @@ calc_models_multi_single <- function(var_names, data, suffix_baseline = "_s0", s
 
   for (var_name in var_names){
     # Mean of relevant repeat
-    data[, paste0("mean_reps_w_data", var_name)] <- calc_mean_reps(var_name = var_name, data = data, suffix_baseline = suffix_baseline, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic)
+    data[, paste0("mean_reps_w_data", var_name)] <- calc_mean_reps(var_name = var_name, data = data, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic)
   }
+
+  # Precision weights for outcome
+  precision_weights <- calc_precision_weights(data = data, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic, precision_weighted = precision_weighted)
+
 
   # Parts of model
   cols <- c(covariates, var_names_base)
@@ -91,7 +100,7 @@ calc_models_multi_single <- function(var_names, data, suffix_baseline = "_s0", s
 
   # Calc model
   form <- as.formula(paste0("cbind(", paste0(outcomes, collapse = ", "), ")~", covs))
-  lambda_lin <- lm(form, data)
+  lambda_lin <- lm(form, data, weights = precision_weights)
 
   # Return models
   return(lambda_lin)
