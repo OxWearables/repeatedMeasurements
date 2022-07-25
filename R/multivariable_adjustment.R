19fa8e32ac4b9117ef6f5ac6e467c942b7f4cf21
R-Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2022-07-25 10:07:28 +0100

    add some internal robustness checks

--- R/multivariable_adjustment.R
+++ R/multivariable_adjustment.R
@@ -105,3 +105,58 @@ calc_models_multi_single <- function(var_names, data, suffix_baseline = "_s0", s
   # Return models
   return(lambda_lin)
 }
+
+
+
+
+
+
+#' Calculates linear models for multivariable adjustment (slow check)
+#'
+#' Calculates linear models for multivariable adjustment using repeated measurements data. Slower version of calc_models_multi, implemented as a check.
+#'
+#' NOTE: uses mean value across all follow-up measurements with data
+#'
+#' @inheritParams calc_models_multi_slow
+#' @return List of models for different outcomes
+#' @export
+calc_models_multi_slow <- function(var_names, data, suffix_baseline = "_s0", suffix_rep = c("_s1", "_s2", "_s3", "_s4"), exclusion_indic = "excluded", covariates = NULL, precision_weighted = FALSE){
+
+  # Checks of inputs
+  var_names_base <- paste0(var_names,  suffix_baseline)
+  var_names_rep <- paste0(var_names, suffix_rep)
+
+  for (var_loc in c(var_names_base, var_names_rep)){
+    if (!(var_loc %in% colnames(data))){
+      stop(paste0(var_loc, " does not appear in data. Is the data set up in the required format?"))
+    }
+  }
+
+  # Prep list of models
+  models_list <- vector("list", length(var_names))
+  names(models_list) <- var_names
+
+  # Parts of models
+  cols <- c(covariates, var_names_base)
+  covs <- paste0(cols, collapse = "+")
+
+  # Precision weights for outcome
+  precision_weights <- calc_precision_weights(data = data, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic, precision_weighted = precision_weighted)
+
+
+  for (var_name in var_names){
+    # Mean of relevant repeat is outcome
+    data[, paste0("mean_reps_w_data", var_name)] <- calc_mean_reps(var_name = var_name, data = data, suffix_rep = suffix_rep, exclusion_indic = exclusion_indic)
+
+    # Calc model
+    form <- as.formula(paste0("mean_reps_w_data", var_name, "~", covs))
+    lamdba_lin <- lm(form, data, weights = precision_weights)
+
+    # Append model to list and remove
+    models_list[[var_name]] <- list(lambda_lin, cols)
+    rm(lambda_lin)
+  }
+
+  # Return models
+  return(models_list)
+}
